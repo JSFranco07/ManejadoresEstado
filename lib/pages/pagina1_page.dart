c5b7fa84ab024dcb58447ebcdde9999da42b567a
Sebastian <joansebasfranco@hotmail.com>
2020-12-10 14:59:46 -0500

    Provider listo

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,13 +1,29 @@
+import 'package:estados/models/usuario.dart';
+import 'package:estados/services/usuario_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Pagina1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final usuarioService = Provider.of<UsuarioService>(context); 
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Página 1'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app), 
+            onPressed: () => usuarioService.removerUsuario()
+          )
+        ],
       ),
-      body: InformacionUsuario(),
+      body: usuarioService.existeUsuario
+        ? InformacionUsuario(usuarioService.usuario)
+        : Center(
+          child: Text('No hay información del usuario'),
+        ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.accessibility_new),
         onPressed: () => Navigator.pushNamed(context, 'pagina2'),
@@ -18,27 +34,37 @@ class Pagina1Page extends StatelessWidget {
 
 class InformacionUsuario extends StatelessWidget {
 
+  final Usuario usuario;
+
+  const InformacionUsuario(this.usuario);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: double.infinity,
       width: double.infinity,
       padding: EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
 
-          Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-          Divider(),
-          ListTile( title: Text('Nombre: ')),
-          ListTile( title: Text('Edad: ')),
-          Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-          Divider(),
-          ListTile( title: Text('Profesión 1')),
-          ListTile( title: Text('Profesión 2')),
-          ListTile( title: Text('Profesión 3')),
+            Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            Divider(),
+            ListTile( title: Text('Nombre: ${usuario.nombre}')),
+            ListTile( title: Text('Edad: ${usuario.edad}')),
+            Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            Divider(),
+            // ListTile( title: Text('Profesión 1')),
+            ...usuario.profesiones.map(
+              (profesion) => ListTile(
+                title: Text(profesion)
+              )
+            ).toList(),
 
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,11 +1,19 @@
+import 'package:estados/models/usuario.dart';
+import 'package:estados/services/usuario_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Pagina2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final usuarioService = Provider.of<UsuarioService>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Página 2'),
+        title: (usuarioService.existeUsuario) 
+          ? Text('${usuarioService.usuario.nombre}')
+          : Text('Página 2')
       ),
       body: Center(
         child: Column(
@@ -16,24 +24,27 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Establecer Usuario', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
               onPressed: (){
-
+                final usuario = Usuario(
+                  nombre: 'Sebastian',
+                  edad: 24,
+                  profesiones: ['Fullstack Developer', 'Jugador competitivo de COD']
+                );
+                usuarioService.usuario = usuario;
               },
             ),
 
             MaterialButton(
               child: Text('Cambiar Edad', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: (){
-
-              },
+              onPressed: () => (usuarioService.existeUsuario)
+                ? usuarioService.cambiarEdad(21)
+                : null,
             ),
 
             MaterialButton(
               child: Text('Añadir Profesión', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: (){
-
-              },
+              onPressed: () => usuarioService.agregarProfesion(),
             ),
 
           ],
