47f8f53ad57fc74a6977ace4fab537549df01b20
Sebastian <joansebasfranco@hotmail.com>
2021-01-13 14:34:15 -0500

    rama Bloc terminada

--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:estados/bloc/usuario/usuario_bloc.dart';
+import 'package:estados/models/usuario.dart';
 
 class Pagina2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // ignore: close_sinks
+    final usuarioBloc = BlocProvider.of<UsuarioBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Página 2'),
@@ -17,6 +24,15 @@ class Pagina2Page extends StatelessWidget {
               color: Colors.blue,
               onPressed: (){
 
+                final newUser = Usuario(
+                  nombre: 'Joan Franco',
+                  edad: 24,
+                  profesiones: ['Flutter mobile developer'],
+                );
+                 
+                usuarioBloc.add(
+                  ActivarUsuario(newUser)
+                );
               },
             ),
 
@@ -24,7 +40,7 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Cambiar Edad', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
               onPressed: (){
-
+                usuarioBloc.add(CambiarEdad(20));
               },
             ),
 
@@ -32,7 +48,7 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Añadir Profesión', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
               onPressed: (){
-
+                usuarioBloc.add(AgregarProfesion('Web Developer'));
               },
             ),
 
